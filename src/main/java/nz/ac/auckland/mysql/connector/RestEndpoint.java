bd9e4631bc22baccdf348d81ac18d6dbe4887cdd
Jack Huang <huangxjack@gmail.com>
2018-07-06 18:07:52 +1200

    added more rest methods

--- src/main/java/nz/ac/auckland/mysql/connector/RestEndpoint.java
+++ src/main/java/nz/ac/auckland/mysql/connector/RestEndpoint.java
@@ -5,15 +5,13 @@ import java.io.InputStream;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import com.sun.jersey.core.header.FormDataContentDisposition;
-
-@Path("/upload")
+@Path("/connect")
 public class RestEndpoint {
 
     Connector _databaseConnector;
@@ -23,5 +21,25 @@ public class RestEndpoint {
     public Response test() {
         return Response.status(Status.OK).entity("OK").build();
     }
-
+    
+    @GET
+    @Path("selectPatient")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response selectByPatient(@QueryParam("pid")String pid,
+    								@QueryParam("queryString")String queryString) {
+    	
+    	String result = _databaseConnector.executeQuery(queryString);
+    	return Response.status(Status.OK).entity(result).build();
+    }
+    
+    @GET
+    @Path("selectEncounter")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response selectByEncounter(@QueryParam("eid")String eid,
+    								@QueryParam("queryString")String queryString) {
+    	
+    	String result = _databaseConnector.executeQuery(queryString);
+    	return Response.status(Status.OK).entity(result).build();
+    }
+  
 }
